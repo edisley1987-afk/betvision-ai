edbf19c1172934790c2c9c0c1894dc10c858b866
edisley1987-afk <edisley1987@gmail.com>
2026-07-13 12:19:15 -0300

    Refactor game analysis functions in analiseJogo.js

--- ai/analiseJogo.js
+++ ai/analiseJogo.js
@@ -1,74 +1,287 @@
-import {
-calcularProbabilidade
-}
-from "./modeloProbabilidade.js";
-
-
-import {
-preverGols
-}
-from "./previsaoGols.js";
+// =======================================
+// BetVision AI
+// Motor de Análise de Jogos
+// =======================================
 
 
 
-export function analisarJogo(dados){
-
-
-    const probabilidade =
-
-    calcularProbabilidade(
-
-        dados.timeCasa,
-
-        dados.timeFora
-
-    );
+export function analisarJogo(jogo){
 
 
 
-    const gols =
+    /*
+       MODELO INICIAL
 
-    preverGols(
+       Futuramente receberá:
 
-        dados.mediaGolsCasa,
+       - histórico equipes
+       - últimos 5 jogos
+       - gols marcados
+       - gols sofridos
+       - mando campo
+       - odds
+       - estatísticas
 
-        dados.mediaGolsFora
+    */
 
-    );
+
+
+    const probabilidadeCasa =
+        calcularCasa();
+
+
+
+    const probabilidadeEmpate =
+        calcularEmpate(
+            probabilidadeCasa
+        );
+
+
+
+    const probabilidadeFora =
+        100 -
+        probabilidadeCasa -
+        probabilidadeEmpate;
+
+
+
+    const golsEsperados =
+        calcularGols();
+
+
+
+    const placar =
+        preverPlacar(
+            golsEsperados
+        );
+
+
+
+    const valueBet =
+        detectarValor(
+            probabilidadeCasa,
+            probabilidadeEmpate,
+            probabilidadeFora
+        );
+
 
 
 
     return {
 
 
-        jogo:
-
-        `${dados.timeCasa.nome}
-        x
-        ${dados.timeFora.nome}`,
+        jogo:jogo.casa 
+            +" x "+
+            jogo.fora,
 
 
-
-        probabilidade,
-
-
-        gols,
+        probabilidadeCasa,
 
 
-        recomendacao:
+        probabilidadeEmpate,
 
-        probabilidade.casa > 65
 
-        ?
+        probabilidadeFora,
 
-        "Casa Favorita"
 
-        :
 
-        "Jogo Equilibrado"
+        golsEsperados,
+
+
+
+        placarPrevisto:
+            placar,
+
+
+
+        valueBet,
+
+
+
+        confianca:
+            calcularConfianca(
+                probabilidadeCasa,
+                probabilidadeFora
+            ),
+
+
+
+        algoritmo:
+            "Probabilidade + Estatística"
+
 
 
     };
+
+
+
+}
+
+
+
+
+
+// =======================================
+// Probabilidade Casa
+// =======================================
+
+function calcularCasa(){
+
+
+    return Math.floor(
+        Math.random() * 
+        (65-35)
+        +35
+    );
+
+
+}
+
+
+
+
+
+// =======================================
+// Empate
+// =======================================
+
+function calcularEmpate(casa){
+
+
+    if(casa > 55){
+
+        return 20;
+
+    }
+
+
+    return 30;
+
+
+}
+
+
+
+
+
+// =======================================
+// Gols Esperados
+// =======================================
+
+function calcularGols(){
+
+
+    return Number(
+        (
+        Math.random()*1.8
+        +0.8
+        )
+        .toFixed(2)
+    );
+
+
+}
+
+
+
+
+
+// =======================================
+// Previsão Placar
+// =======================================
+
+function preverPlacar(gols){
+
+
+
+    if(gols < 1.5){
+
+        return "1x0";
+
+    }
+
+
+
+    if(gols < 2.5){
+
+        return "2x1";
+
+    }
+
+
+
+    return "2x2";
+
+
+}
+
+
+
+
+
+// =======================================
+// Detector Value Bet
+// =======================================
+
+function detectarValor(
+    casa,
+    empate,
+    fora
+){
+
+
+
+    const maior =
+        Math.max(
+            casa,
+            empate,
+            fora
+        );
+
+
+
+    return maior >= 55;
+
+
+
+}
+
+
+
+
+
+// =======================================
+// Confiança IA
+// =======================================
+
+function calcularConfianca(
+    casa,
+    fora
+){
+
+
+    const diferenca =
+        Math.abs(
+            casa - fora
+        );
+
+
+    if(diferenca >=30){
+
+        return "Alta";
+
+    }
+
+
+    if(diferenca >=15){
+
+        return "Média";
+
+    }
+
+
+
+    return "Baixa";
 
 
 }
